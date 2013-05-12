691e95568e1a4a0d9c589a1ded96da710162dfc7
Seb <sllabres@gmail.com>
2013-05-12 20:12:58 +0100

    minor change to trigger build

--- AirFair/Tests/HelloWorld.js
+++ AirFair/Tests/HelloWorld.js
@@ -2,7 +2,6 @@
 ///<reference path="/HelloWorld.js"/>
 
 QUnit.module("Hello World Tests");
-
 test("When calling hello Then 'Hello World!' is returned", function () {
     var helloWorld = new HelloWorld();    
     var actualValue = helloWorld.Hello();    
