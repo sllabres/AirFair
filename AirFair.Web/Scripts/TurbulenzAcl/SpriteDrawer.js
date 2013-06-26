ec20f6e7ed10c346655d713dac677bb799981771
Seb <sllabres@gmail.com>
2013-06-26 21:45:31 +0100

    meh

---
+++ AirFair.Web/Scripts/TurbulenzAcl/SpriteDrawer.js
@@ -0,0 +1,38 @@
+///<reference path="SpriteLoader.ts"/>
+///<reference path="..\Game.ts"/>
+///<reference path="..\EventBus.ts"/>
+///<reference path="..\..\Turbulenz\draw2d.ts"/>
+///<reference path="..\..\Turbulenz\webgl\turbulenzengine.ts"/>
+var TurbulenzAcl;
+(function (TurbulenzAcl) {
+    var SpriteDrawer = (function () {
+        function SpriteDrawer(graphicsDevice, draw2D, spriteRepository, observer) {
+            this.graphicsDevice = graphicsDevice;
+            this.draw2D = draw2D;
+            this.spriteRepository = spriteRepository;
+            this.observer = observer;
+            var _this = this;
+            this.drawBackgroundProxy = function (background) {
+                _this.draw(_this.spriteRepository.getBy());
+            };
+            this.observer.subscribe(AirFair.event.drawBackground, this.drawBackgroundProxy);
+        }
+        SpriteDrawer.prototype.draw = function (sprite) {
+            if(this.graphicsDevice.beginFrame()) {
+                this.graphicsDevice.clear([
+                    0, 
+                    0, 
+                    0, 
+                    0
+                ], 1.0);
+                this.draw2D.begin();
+                this.draw2D.drawSprite(sprite);
+                this.draw2D.end();
+                this.graphicsDevice.endFrame();
+            }
+        };
+        return SpriteDrawer;
+    })();
+    TurbulenzAcl.SpriteDrawer = SpriteDrawer;    
+})(TurbulenzAcl || (TurbulenzAcl = {}));
+//@ sourceMappingURL=SpriteDrawer.js.map
---
+++ AirFair.Web/Scripts/TurbulenzAcl/SpriteLoader.js
@@ -0,0 +1,71 @@
+///<reference path="..\..\Turbulenz\texturemanager.ts"/>
+///<reference path="..\..\Turbulenz\draw2d.ts"/>
+///<reference path="..\..\Turbulenz\webgl\turbulenzengine.ts"/>
+///<reference path="..\EventBus.ts"/>
+var TurbulenzAcl;
+(function (TurbulenzAcl) {
+    var SpriteRepository = (function () {
+        function SpriteRepository(observer) {
+            this.observer = observer;
+            var _this = this;
+            this.spriteCreatedProxy = function (sprite) {
+                _this.sprite = sprite;
+                _this.observer.notify(AirFair.event.spriteLoadComplete, null);
+            };
+            this.observer.subscribe(AirFair.event.spriteCreated, this.spriteCreatedProxy);
+        }
+        SpriteRepository.prototype.getBy = function () {
+            return this.sprite;
+        };
+        return SpriteRepository;
+    })();
+    TurbulenzAcl.SpriteRepository = SpriteRepository;    
+    var SpriteLoader = (function () {
+        function SpriteLoader(draw2D, graphicsDevice, observer) {
+            this.draw2D = draw2D;
+            this.graphicsDevice = graphicsDevice;
+            this.observer = observer;
+            var _this = this;
+            this.textureLoadProxy = function (texture) {
+                if(texture) {
+                    observer.notify(AirFair.event.spriteCreated, _this.createSprite(texture));
+                }
+            };
+        }
+        SpriteLoader.prototype.loadAll = function () {
+            this.loadTexture("Sky");
+        };
+        SpriteLoader.prototype.createSprite = function (texture) {
+            var sprite = Draw2DSprite.create({
+                width: this.graphicsDevice.width,
+                height: this.graphicsDevice.height,
+                x: this.graphicsDevice.width / 2,
+                y: this.graphicsDevice.height / 2,
+                color: [
+                    1.0, 
+                    1.0, 
+                    1.0, 
+                    1.0
+                ]
+            });
+            sprite.setTexture(texture);
+            sprite.setTextureRectangle([
+                0, 
+                0, 
+                texture.width, 
+                texture.height
+            ]);
+            return sprite;
+        };
+        SpriteLoader.prototype.loadTexture = function (name) {
+            var texture = this.graphicsDevice.createTexture({
+                src: "Assets/Sky.jpg",
+                mipmaps: true,
+                onload: this.textureLoadProxy
+            });
+        };
+        return SpriteLoader;
+    })();
+    TurbulenzAcl.SpriteLoader = SpriteLoader;    
+})(TurbulenzAcl || (TurbulenzAcl = {}));
+//@ sourceMappingURL=SpriteLoader.js.map
